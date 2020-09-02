e9c73a1dde612294de3de7447dec98d646e68f93
lburk87 <lburk87@gmail.com>
2020-09-02 08:47:51 -0500

    5 tests on existing sourcecode

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -7,9 +8,30 @@ import static org.junit.Assert.*;
 public class BalancedBracketsTest {
 
     //TODO: add tests here
+
     @Test
-    public void emptyTest() {
-        assertEquals(true, true);
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+
+    @Test
+    public void noBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("words"));
+    }
+
+    @Test
+    public void balancedBracketsAndWordsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[words]and bracket[ and bracket ]"));
+    }
+
+    @Test
+    public void notBalancedBracketsAndWordsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[words[ whoa [ gotta close these brackets]] whoops"));
+    }
+
+    @Test
+    public void bracketsArePairedCorrectly() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("][[["));
     }
 
 
